ad8d8181ea2c1eb9956094676e2d464ef6936031
CreativeGrape <38544604+CreativeGrape@users.noreply.github.com>
2018-04-25 21:53:35 +0200

    Add files via upload

--- PyBMICalc.py
+++ PyBMICalc.py
@@ -1,35 +1,26 @@
 #DIALOG
-
 pl_lang = "Wybrano język polski."
 en_lang = "You've picked English."
-
 pl_uni_metric = "Wybrano system metryczny."
 pl_uni_imperial = "Wybrano system imperialny."
-
 en_uni_metric = "You've picked metric system."
 en_uni_imperial = "You've picked imperial system."
-
 pl_again = "Spróbuj ponownie!"
 en_again = "Try again!"
 again = "Try again! / Spróbuj ponownie!"
-
 lg_input = "English[EN]/ Polski[PL]: "
 pl_uni_input = "Metryczny[M]/ Imperialny[I]: "
 en_uni_input = "Metric[M]/ Imperial[I]: "
-
 pl_wm = "Waga[kg]: "
 pl_hm = "Wzrost[cm]: "
 pl_wi = "Waga[lb]: "
 pl_hi = "Wzrost[ft]: "
-
 en_wm = "Weight[kg]: "
 en_hm = "Height[cm]: "
 en_wi = "Weight[lb]: "
 en_hi = "Height[ft]: "
-
 pl_bmi_print_dialog = "Twój wskaźnik masy ciała wynosi: "
 en_bmi_print_dialog = "Your body mass index is: "
-
 pl_list = """
 <15 Wygłodzenie
 15-16 Wychudzenie
@@ -39,7 +30,6 @@ pl_list = """
 30-35 I stopień otyłości
 35-40 II stopień otyłości (otyłość kliniczna)
 >40 III stopień otyłości (otyłość skrajna)"""
-
 pl_1 = "Wygłodzenie"
 pl_2 = "Wychudzenie"
 pl_3 = "Niedowaga"
@@ -48,7 +38,6 @@ pl_5 = "Nadwaga"
 pl_6 = "I stopień otyłości"
 pl_7 = "II stopień otyłości (otyłość kliniczna)"
 pl_8 = "III stopień otyłości (otyłość skrajna)"
-
 en_list = """
 <15 Very severely underweight
 15-16 Severely underweight
@@ -58,7 +47,6 @@ en_list = """
 30-35 Obese Class I (Moderately obese)
 35-40 Obese Class II (Severely obese)
 >40 Obese Class III (Very severely obese)"""
-
 en_1 = "Very severely underweight"
 en_2 = "Severely underweight"
 en_3 = "Underweight"
@@ -67,17 +55,20 @@ en_5 = "Overweight"
 en_6 = "Obese Class I (Moderately obese)"
 en_7 = "Obese Class II (Severely obese)"
 en_8 = "Obese Class III (Very severely obese)"
-
 sum_pl = "Twój wynik to: "
 sum_en = "You're: "
-
 #VARIABLES
-
 lg = "lg"
 uni = "uni"
-
+#COLORS
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
 #DEF
-
 def lang():
     global lg
     lg = input(lg_input)
@@ -93,7 +84,6 @@ def lang():
     else:
         print(again)
         lang()
-
 def pl_units():
     global uni
     uni = input(pl_uni_input)
@@ -109,7 +99,6 @@ def pl_units():
     else:
         print(pl_again)
         pl_units()
-
 def en_units():
     global uni
     uni = input(en_uni_input)
@@ -125,7 +114,6 @@ def en_units():
     else:
         print(en_again)
         en_units()
-
 def pl_data_m():
     global w
     global h
@@ -134,7 +122,6 @@ def pl_data_m():
     if not w.isnumeric() or not h.isnumeric():
         print(pl_again)
         pl_data_m()
-
 def pl_data_i():
     global w
     global h
@@ -143,7 +130,6 @@ def pl_data_i():
     if not w.isnumeric() or not h.isnumeric():
         print(pl_again)
         pl_data_i()
-
 def en_data_m():
     global w
     global h
@@ -152,7 +138,6 @@ def en_data_m():
     if not w.isnumeric() or not h.isnumeric():
         print(en_again)
         en_data_m()
-
 def en_data_i():
     global w
     global h
@@ -161,17 +146,14 @@ def en_data_i():
     if not w.isnumeric() or not h.isnumeric():
         print(en_again)
         en_data_i()
-
 def m_bmi(w, h):
     global bmi
     bmi = (float(w) / ((float(h)) / 100) ** 2)
-
 def i_bmi(w, h):
     global bmi
     bmi = (float(w) / float(h) ** 2) * 703
-
 def pl_0():
-    if bmi >= 40.:
+    if bmi > 40.:
         return pl_8
     elif bmi >= 35.:
         return pl_7
@@ -185,13 +167,12 @@ def pl_0():
         return pl_3
     elif bmi >= 15.:
         return pl_2
-    elif bmi <= 15.:
+    elif bmi < 15.:
         return pl_1
     else:
         return "Error"
-
 def en_0():
-    if bmi >= 40.:
+    if bmi > 40.:
         return en_8
     elif bmi >= 35.:
         return en_7
@@ -205,41 +186,31 @@ def en_0():
         return en_3
     elif bmi >= 15.:
         return en_2
-    elif bmi <= 15.:
+    elif bmi < 15.:
         return en_1
     else:
         return "Error"
-
 def summary_pl():
-    print("%s %s" % (sum_pl, pl_0()))
-
+    print("%s%s" % (sum_pl, pl_0()))
 def summary_en():
-    print("%s %s" % (sum_en, en_0()))
-
+    print("%s%s" % (sum_en, en_0()))
 def print_bmi_pl():
     print(pl_bmi_print_dialog + "%.2f" % (bmi))
     (summary_pl())
-
 def print_bmi_en():
     print(en_bmi_print_dialog + "%.2f" % (bmi))
     (summary_en())
 
 #CODE
-
 print("""PyBMICalc - 2018
 """)
-
 lang()
-
 print()
-
 if lg == "pl":
     pl_units()
 elif lg == "en":
     en_units()
-
 print()
-
 if lg == "pl":
     if uni == "m":
         pl_data_m()
@@ -250,14 +221,11 @@ elif lg == "en":
         en_data_m()
     elif uni == "i":
         en_data_i()
-
 print()
-
 if uni == "m":
     m_bmi(w, h)
 elif uni == "i":
     i_bmi(w, h)
-
 if lg == "pl":
     print_bmi_pl()
     print(pl_list)
